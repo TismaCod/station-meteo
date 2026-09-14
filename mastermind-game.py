b3bd81d42ecfa63a0097e0f1ba671a5ff7df661f
Ms. 👾 <tisma9411@gmail.com>
2026-09-14 14:24:57 +0200

    Update mastermind-game.py

--- mastermind-game.py
+++ mastermind-game.py
@@ -1,5 +1,12 @@
 import random
-passwd = str(random.randint(1000,9999))
+
+niv = input("Choisir le niveau : ")
+if niv == "1":
+        passwd = str(random.randint(100,999))
+if niv == "2":
+        passwd = str(random.randint(1000,9999))
+if niv == "3":
+        passwd = str(random.randint(10000,99999))
 print(passwd)
 
 def char_checker(n,v):
@@ -15,11 +22,16 @@ def char_checker(n,v):
                 incorrect += 1
     return correct,incorrect
 
+find = False
 for i in range(10):
     guess = input("\nEntrer votre proposition : ")
     check = char_checker(passwd,guess)
     if check[1] == 0:
         print("\nBRAVO !\n")
+        find = True
         break
     else:
         print("Chiffres bien places : ",check[0],"Chiffres mal places : ",check[1])
+
+if find == False:
+    print("PERDU ! Le code etait ",passwd)
